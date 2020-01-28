8609950aa2ce2e45b9879ea9492bf9b8377d0ef7
Ed Guild <ed@vizzie.com>
2020-01-28 14:21:24 -0500

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -2,6 +2,7 @@
 #include "esp_event.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/semphr.h"
 #include "driver/timer.h"
 #include "nvs_flash.h"
 #include "protocol_examples_common.h"
@@ -19,70 +20,89 @@ char* if_indextoname(unsigned int ifindex, char* ifname)
   return nullptr;
 }
 
-static intr_handle_t s_link_timer_handle;
-
-void high_priority_output_task(void* arg)
+void IRAM_ATTR timer_HighPriority_isr(void* user_param)
 {
-  auto pLink = static_cast<ableton::Link*>(arg);
+  static BaseType_t xHigherPriorityTaskWoken = pdFALSE;
 
-  //Reset irq and set for next time
   TIMERG0.int_clr_timers.t0 = 1;
   TIMERG0.hw_timer[0].config.alarm_en = 1;
 
-  //Do the Link task
-  bool clockTriggered = false;
-  const uint8_t ppqn = 4;
-  const double quantum = 4.0;
-
-  auto state = pLink->captureAudioSessionState();
-  const auto now = pLink->clock().micros();
-  const auto phase = state.phaseAtTime(now, quantum);
-  const double clockPhase = (phase - (int)phase) * ppqn;
-
-  if ((clockPhase - (int)clockPhase) < 0.1) {
-    clockTriggered = true;
-  };
-
-  gpio_set_level(LED, clockTriggered);
-
-  //changing the state
-  state.setTempo(138.0, now);
-
-  //causes a crash here
-  pLink->commitAudioSessionState(state);
-
-
+  xSemaphoreGiveFromISR(user_param, &xHigherPriorityTaskWoken);
+  if (xHigherPriorityTaskWoken)
+  {
+    portYIELD_FROM_ISR();
+  }
 }
 
-void timer_tg0_initialise (int timer_period_us, void *data)
+
+void timer_group0_init(int timer_period_us, void* user_param)
 {
     timer_config_t config = {
-            .alarm_en = true,				//Alarm Enable
-            .counter_en = false,			//If the counter is enabled it will start incrementing / decrementing immediately after calling timer_init()
-            .intr_type = TIMER_INTR_LEVEL,	//Is interrupt is triggered on timer’s alarm (timer_intr_mode_t)
-            .counter_dir = TIMER_COUNT_UP,	//Does counter increment or decrement (timer_count_dir_t)
-            .auto_reload = true,			//If counter should auto_reload a specific initial value on the timer’s alarm, or continue incrementing or decrementing.
-            .divider = 80   				//Divisor of the incoming 80 MHz (12.5nS) APB_CLK clock. E.g. 80 = 1uS per timer tick
+            .alarm_en = true,
+            .counter_en = false,
+            .intr_type = TIMER_INTR_LEVEL,
+            .counter_dir = TIMER_COUNT_UP,
+            .auto_reload = true,
+            .divider = 80
     };
 
     timer_init(TIMER_GROUP_0, TIMER_0, &config);
     timer_set_counter_value(TIMER_GROUP_0, TIMER_0, 0);
     timer_set_alarm_value(TIMER_GROUP_0, TIMER_0, timer_period_us);
     timer_enable_intr(TIMER_GROUP_0, TIMER_0);
-    timer_isr_register(TIMER_GROUP_0, TIMER_0, &high_priority_output_task, data, 0, &s_link_timer_handle);
-
+    timer_isr_register(TIMER_GROUP_0, TIMER_0, &timer_HighPriority_isr, user_param, 0, nullptr);
     timer_start(TIMER_GROUP_0, TIMER_0);
 }
 
-void link_task(void* user_param) {
-  auto pLink = static_cast<ableton::Link*>(user_param);
+void low_priority_task(void* user_param)
+{
+  auto link = static_cast<ableton::Link*>(user_param);
+  double quantum = 4.0;
 
-  // START HIGH-PRIORITY INTERRUPT
-  timer_tg0_initialise(100, user_param);
+  while (true)
+  {
+    auto state = link->captureAppSessionState();
+    std::size_t numPeers = link->numPeers();
+    auto now = link->clock().micros();
+    auto beats = state.beatAtTime(now, quantum);
+    auto phase = state.phaseAtTime(now, quantum);
+    auto tempo = state.tempo();
 
-  while (true) {
-    // do low priority things like update UI
+    printf("Peers: %d | tempo: %d | beats: %f | ", numPeers, (int)tempo, beats);
+    for (int i = 0; i < ceil(quantum); ++i)
+    {
+      if (i < phase)
+      {
+        printf("X");
+      }
+      else
+      {
+        printf("O");
+      }
+    }
+    printf("\n");
     vTaskDelay(10 / portTICK_PERIOD_MS);
+  }
+}
+
+void high_priority_task(void* user_param)
+{
+  ableton::Link link(120.0f);
+  link.enable(true);
+
+  gpio_set_direction(LED, GPIO_MODE_OUTPUT);
+
+  xTaskCreatePinnedToCore(
+    low_priority_task, "low-priority", 8192, &link, 1, nullptr, 1);
+
+  while (true)
+  {
+    if (xSemaphoreTake(user_param, portMAX_DELAY) == pdTRUE)
+    {
+      const auto state = link.captureAudioSessionState();
+      const auto phase = state.phaseAtTime(link.clock().micros(), 1.);
+      gpio_set_level(LED, fmodf(phase, 1.) < 0.1);
+    }
   }
 }
 
@@ -93,16 +113,15 @@ extern "C" void app_main()
   ESP_ERROR_CHECK(esp_event_loop_create_default());
   ESP_ERROR_CHECK(example_connect());
 
-  gpio_set_direction(LED, GPIO_MODE_OUTPUT);
-
-  ableton::Link link(120.0f);
-  link.enable(true);
+  SemaphoreHandle_t hi_priority_semphr = xSemaphoreCreateBinary();
+  timer_group0_init(100, hi_priority_semphr);
 
   xTaskCreatePinnedToCore(
-    link_task, "link_task", 8192, static_cast<void*>(&link), 10, nullptr, 1);
+    high_priority_task, "hi-priority", 8192, hi_priority_semphr, 1, nullptr, 1);
 
   while (true)
   {
     ableton::link::platform::IoContext::poll();
+    vTaskDelay(1);
   }
 }
